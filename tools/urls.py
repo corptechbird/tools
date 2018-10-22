10ef6841629c8eaf90665a7d946e53a7d311b3b6
corptechbird <corp.techbird@gmail.com>
2018-10-23 05:57:46 +0900

    Update pattern

--- tools/urls.py
+++ tools/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'app'
 
@@ -8,4 +10,4 @@ urlpatterns = [
     path('cross/', views.cross_index, name='cross_index'),
     path('pivot/', views.cross_index, name='pivot_index'),
     path('export/', views.post_export, name='export'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
